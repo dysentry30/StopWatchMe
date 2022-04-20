252543e3357e0a88cac273ad642778c4af44aa40
dysentry30 <bagassatrianurwinanto@gmail.com>
2022-04-21 04:39:42 +0700

    Updated Version 1.1 (Redesign)

--- index.js
+++ index.js
@@ -3,30 +3,61 @@ overlay.style.opacity = 0;
 
 let pickTime = []
 const alarm = new Audio("assets/Alarm Sound Effect.mp3");
-pickTime = prompt("Type your second, minute and hour seperate with ','.\nNote: if this prompt popup twice, just do the input.\nMake sure tap or click the web document for better experience.").trim().split(",").map(val => Number(val));
-let sec = pickTime[0];
-let min = pickTime[1];
-let hour = pickTime[2];
+let sec = 0;
+let min = 0;
+let hour = 0;
+inputPrompt();
+function inputPrompt() {
+    pickTime = prompt("Type your second, minute and hour seperate with ','.\nNote: if this prompt popup twice, just do the input.\nMake sure tap or click the web document for better experience.").trim().split(",").map(val => Number(val));
 
-const rectFillSec = document.querySelector(".sec .rect .fill");
+    sec = pickTime[0];
+    min = pickTime[1];
+    hour = pickTime[2];
+    const { msg, error } = checkValidation(sec, min, hour);
+    if (error) {
+        confirm("Error:\n" + msg.join(",\n"));
+        inputPrompt();
+    }
+}
+function checkValidation(sec, min, hour) {
+    let errorMsg = {
+        msg: [],
+        error: false,
+    }
+    if (sec < 1 || sec > 59) {
+        errorMsg.msg.push("Second must be greater than 0 and less than 60");
+        errorMsg.error = true;
+    };
+    if (min < 0 || min > 60) {
+        errorMsg.msg.push("Minute must be greater or equal than 0 and less or equal than 60");
+        errorMsg.error = true;
+    };
+    if (hour < 0 || hour > 60) {
+        errorMsg.msg.push("Hour must be greater or equal than 0 and less or equal than 60");
+        errorMsg.error = true;
+    };
+    return errorMsg;
+}
+
+// const rectFillSec = document.querySelector(".sec .rect .fill");
 const textSec = document.querySelector(".sec h3 b");
-const rectFillSecStyle = window.getComputedStyle(rectFillSec);
+// const rectFillSecStyle = window.getComputedStyle(rectFillSec);
 
-const rectFillMin = document.querySelector(".min .rect .fill");
+// const rectFillMin = document.querySelector(".min .rect .fill");
 const textMin = document.querySelector(".min h3 b");
-const rectFillMinStyle = window.getComputedStyle(rectFillMin);
+// const rectFillMinStyle = window.getComputedStyle(rectFillMin);
 
-const rectFillHour = document.querySelector(".hour .rect .fill");
+// const rectFillHour = document.querySelector(".hour .rect .fill");
 const textHour = document.querySelector(".hour h3 b");
-const rectFillHourStyle = window.getComputedStyle(rectFillHour);
+// const rectFillHourStyle = window.getComputedStyle(rectFillHour);
 
 const resetBtn = document.querySelector("#reset");
 const newTimerBtn = document.querySelector("#new-timer");
 const stopBtn = document.querySelector("#stop");
 
-const topSecFill = rectFillSecStyle["top"].replace("px", "");
-const topMinFill = rectFillMinStyle["top"].replace("px", "");
-const topHourFill = rectFillHourStyle["top"].replace("px", "");
+// const topSecFill = rectFillSecStyle["top"].replace("px", "");
+// const topMinFill = rectFillMinStyle["top"].replace("px", "");
+// const topHourFill = rectFillHourStyle["top"].replace("px", "");
 
 
 // const overlay = document.querySelector(".overlay");
@@ -64,18 +95,18 @@ let rectFillCounter = (sec, min, hour) => {
     textSec.textContent = secCounter;
     textMin.textContent = minCounter;
     textHour.textContent = hourCounter;
+    // console.log(topSecFill);
+    // let topPositionRectSec = (sec / 100) * topSecFill;
+    // let topPositionRectMin = (min / 100) * topMinFill;
+    // let topPositionRectHour = (hour / 100) * topHourFill;
 
-    let topPositionRectSec = (sec / 100) * topSecFill;
-    let topPositionRectMin = (min / 100) * topMinFill;
-    let topPositionRectHour = (hour / 100) * topHourFill;
+    // const stepSecCounter = Math.abs(topPositionRectSec / sec);
+    // const stepMinCounter = Math.abs(topPositionRectMin / min);
+    // const stepHourCounter = Math.abs(topPositionRectHour / hour);
 
-    const stepSecCounter = Math.abs(topPositionRectSec / sec);
-    const stepMinCounter = Math.abs(topPositionRectMin / min);
-    const stepHourCounter = Math.abs(topPositionRectHour / hour);
-
-    rectFillSec.style.top = topPositionRectSec + "px";
-    rectFillMin.style.top = topPositionRectMin + "px";
-    rectFillHour.style.top = topPositionRectHour + "px";
+    // rectFillSec.style.top = topPositionRectSec + "px";
+    // rectFillMin.style.top = topPositionRectMin + "px";
+    // rectFillHour.style.top = topPositionRectHour + "px";
 
     let counter = setInterval(() => {
         if (secCounter < 1 && minCounter < 1 && hourCounter < 1) {
@@ -89,38 +120,38 @@ let rectFillCounter = (sec, min, hour) => {
         if (secCounter < 1) {
             secCounter = 60;
             textSec.textContent = secCounter;
-            topPositionRectSec = -191;
-            rectFillSec.style.top = topPositionRectSec + "px";
+            // topPositionRectSec = -191;
+            // rectFillSec.style.top = topPositionRectSec + "px";
 
-            // Min Div
-            topPositionRectMin = Number(topPositionRectMin) + stepMinCounter;
+            // ! Min Div
             minCounter--;
             textMin.textContent = minCounter;
-            rectFillMin.style.top = topPositionRectMin + "px";
+            // topPositionRectMin = Number(topPositionRectMin) + stepMinCounter;
+            // rectFillMin.style.top = topPositionRectMin + "px";
 
-            // clearInterval(rectSecFillCounter);
+            // clearInterval(rectSecFillCounter);   
             // return;
             if (minCounter < 0) {
                 minCounter = 59;
                 textMin.textContent = minCounter;
-                topPositionRectMin = -191;
-                rectFillMin.style.top = topPositionRectMin + "px";
+                // topPositionRectMin = -191;
+                // rectFillMin.style.top = topPositionRectMin + "px";
 
                 if (hourCounter > 0) {
-                    topPositionRectHour = Number(topPositionRectHour) + stepHourCounter;
                     hourCounter--;
                     textHour.textContent = hourCounter;
-                    rectFillHour.style.top = topPositionRectHour + "px";
+                    // topPositionRectHour = Number(topPositionRectHour) + stepHourCounter;
+                    // rectFillHour.style.top = topPositionRectHour + "px";
                 }
                 // clearInterval(rectMinFillCounter);
                 // return;
             };
         }
-        // Sec Div
-        topPositionRectSec = Number(topPositionRectSec) + stepSecCounter;
+        // ! Sec Div
+        // topPositionRectSec = Number(topPositionRectSec) + stepSecCounter;
         secCounter--;
         textSec.textContent = secCounter;
-        rectFillSec.style.top = topPositionRectSec + "px";
+        // rectFillSec.style.top = topPositionRectSec + "px";
 
 
     }, 1000);
